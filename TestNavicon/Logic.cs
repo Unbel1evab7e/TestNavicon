dd138053d1deafe7bddf843d01097c7e38ec1707
Unbel1evab7e <unbel1evab7e@gmail.com>
2021-07-27 12:09:26 +0700

    fix variables adn addd comments

--- TestNavicon/Logic.cs
+++ TestNavicon/Logic.cs
@@ -18,6 +18,7 @@ namespace TestNavicon
      
         public async Task<string> GetImages(string url, int threadCount, int imageCount)
         {
+            //Объявление всех переменных
             WebClient client = new WebClient();
             string data="";
             Regex regex = new Regex(@"\<img.+?src=\""(?<imgsrc>.+?)\"".+?\>", RegexOptions.ExplicitCapture);
@@ -25,23 +26,27 @@ namespace TestNavicon
             string directory = "Images";
             List<JsonResponse> jsonResponse = new List<JsonResponse>();
             string host;
+            string path;
+            string savePath;
+            JsonResponse tempCase;
             FileInfo fileInfo;
             var endTime = DateTime.Now.AddMinutes(1);
-
+            //Проверка валидности параметра потоков
             if (threadCount > Environment.ProcessorCount)
                 threadCount = Environment.ProcessorCount;
             if (threadCount <= 0)
                 threadCount = Environment.ProcessorCount;
-
+            //Создание папки для хранения фотографий
             if (!Directory.Exists(directory))
             {
                 Directory.CreateDirectory(directory);
             }
-
+            //Основная логика
             try
             {
+                //Создаём переменную для того чтобы выделить хост
                 var uri = new UriBuilder(url);
-
+                //Скачиваем html страницу в текстовом виде
                 using (Stream data1 = client.OpenRead(url))
 
                 {
@@ -50,60 +55,65 @@ namespace TestNavicon
                         data = reader.ReadToEnd();
                     }
                 }
-
+                //Парсим страницу на нахождение img тэгов
                 MatchCollection matches = regex.Matches(data);
                 var imagesUrl = matches
                     .Cast<Match>()
                     .Select(m => m.Groups["imgsrc"].Value).ToList();
-
+                //Проверяем на валидность параметр количества картинок
                 if (imageCount > imagesUrl.Count)
                     imageCount = imagesUrl.Count;
                 if (imageCount <= 0)
                     imageCount = imagesUrl.Count;
-                
+                //Оптимизируем количество потоков
                 threadCount = (imageCount / 10 + threadCount) / 2;
                 if (threadCount < 1)
                     threadCount = 1;
                 if (imageCount < threadCount)
                     threadCount = imageCount;
+                //Логика скачивания 
                 imagesUrl.GetRange(0, imageCount).AsParallel()
                     .WithDegreeOfParallelism(threadCount)
                     .ForAll(value =>
                 {
+                    //Проверям не прошла ли одна минута
                     if(DateTime.Now<=endTime)
                     {
-                        
-
+                        //Проверям нашлось ли имя с помощью регулярки
                         Match match = regexImg.Match(value);
                         if (match.Success)
                         {
-                            var path = value;
+                            path = value;
                             string name = match.Groups["name"].Value;
-
+                            //Находим имя и тэг нашей фотографии
                             if (path[0] == '/')
                             {
                                 path = "https://" + uri.Host + path;
                                 host = uri.Host;
 
                             }
+
                             else
                                 host = new UriBuilder(value).Host;
-                            string savePath = Path.Combine(directory, name);
+                            //Создаём путь хранения фотографии
+                            savePath = Path.Combine(directory, name);
+                            //Скачиваем фотографию
                             using (WebClient localClient = new WebClient())
                             {
                                 localClient.DownloadFile(path, savePath);
                             }
-
+                            //Добавляем информацию в класс который потом будем преобразовывать в json
                             if (jsonResponse.Any(x => x.Host == host))
                             {
                                 fileInfo = new FileInfo(savePath);
-                                var tempCase = jsonResponse.Where(x => x.Host == host).ElementAt(0);
+                                tempCase = jsonResponse.Where(x => x.Host == host).ElementAt(0);
                                 tempCase.Images = tempCase.Images.Concat(new ImageInfo[] { new ImageInfo { Alt = value, Src = name, Size = fileInfo.Length } }).ToArray();
                             }
+
                             else
                             {
                                 fileInfo = new FileInfo(savePath);
-                                var tempCase = new JsonResponse { Host = host };
+                                tempCase = new JsonResponse { Host = host };
                                 tempCase.Images = new ImageInfo[] { new ImageInfo { Alt = value, Src = name, Size = fileInfo.Length } };
 
                                 jsonResponse.Add(tempCase);
--- TestNavicon/Program.cs
+++ TestNavicon/Program.cs
@@ -9,7 +9,7 @@ namespace TestNavicon
         static async Task Main(string[] args)
         {
             Logic logic = new Logic();
-            var a =await logic.GetImages("https://ru.depositphotos.com/239952804/stock-photo-high-angle-view-young-sportsman.html", 1, 2);
+            var a =await logic.GetImages("https://ru.depositphotos.com/239952804/stock-photo-high-angle-view-young-sportsman.html", 45, 0);
         }
       
     }
